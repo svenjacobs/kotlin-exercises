7e75c4fc5437be70d14430298db1b1b77b3bbccc
Sven Jacobs <>
2025-11-11 11:19:37 +0100

    solution CounterContext

--- src/main/kotlin/coroutines/context/CounterContext.kt
+++ src/main/kotlin/coroutines/context/CounterContext.kt
@@ -6,60 +6,67 @@ import java.util.concurrent.atomic.AtomicInteger
 import kotlin.coroutines.*
 import kotlin.test.assertEquals
 
-// TODO
+class CounterContext : AbstractCoroutineContextElement(CounterContext) {
 
-//fun main(): Unit = runBlocking(CounterContext()) {
-//    println(coroutineContext[CounterContext]?.next()) // 0
-//    println(coroutineContext[CounterContext]?.next()) // 1
-//    launch {
-//        println(coroutineContext[CounterContext]?.next())// 2
-//        println(coroutineContext[CounterContext]?.next())// 3
-//    }
-//    launch(CounterContext()) {
-//        println(coroutineContext[CounterContext]?.next())// 0
-//        println(coroutineContext[CounterContext]?.next())// 1
-//    }
-//}
+    private val number = AtomicInteger(0)
+
+    fun next(): Int = number.getAndIncrement()
+
+    companion object Key : CoroutineContext.Key<CounterContext>
+}
+
+fun main(): Unit = runBlocking(CounterContext()) {
+    println(coroutineContext[CounterContext]?.next()) // 0
+    println(coroutineContext[CounterContext]?.next()) // 1
+    launch {
+        println(coroutineContext[CounterContext]?.next())// 2
+        println(coroutineContext[CounterContext]?.next())// 3
+    }
+    launch(CounterContext()) {
+        println(coroutineContext[CounterContext]?.next())// 0
+        println(coroutineContext[CounterContext]?.next())// 1
+    }
+}
 
 class CounterContextTests {
-//    @Test
-//    fun `should return next numbers in the same coroutine`() = runBlocking<Unit>(CounterContext()) {
-//        assertEquals(0, coroutineContext[CounterContext]?.next())
-//        assertEquals(1, coroutineContext[CounterContext]?.next())
-//        assertEquals(2, coroutineContext[CounterContext]?.next())
-//        assertEquals(3, coroutineContext[CounterContext]?.next())
-//        assertEquals(4, coroutineContext[CounterContext]?.next())
-//    }
-//
-//    @Test
-//    fun `should have independent counter for each instance`() = runBlocking<Unit> {
-//        launch(CounterContext()) {
-//            assertEquals(0, coroutineContext[CounterContext]?.next())
-//            assertEquals(1, coroutineContext[CounterContext]?.next())
-//            assertEquals(2, coroutineContext[CounterContext]?.next())
-//        }
-//        launch(CounterContext()) {
-//            assertEquals(0, coroutineContext[CounterContext]?.next())
-//            assertEquals(1, coroutineContext[CounterContext]?.next())
-//            assertEquals(2, coroutineContext[CounterContext]?.next())
-//        }
-//    }
-//
-//    @Test
-//    fun `should propagate to the child`() = runBlocking<Unit>(CounterContext()) {
-//        assertEquals(0, coroutineContext[CounterContext]?.next())
-//        launch {
-//            assertEquals(1, coroutineContext[CounterContext]?.next())
-//            launch {
-//                assertEquals(2, coroutineContext[CounterContext]?.next())
-//            }
-//            launch(CounterContext()) {
-//                assertEquals(0, coroutineContext[CounterContext]?.next())
-//                assertEquals(1, coroutineContext[CounterContext]?.next())
-//                launch {
-//                    assertEquals(2, coroutineContext[CounterContext]?.next())
-//                }
-//            }
-//        }
-//    }
+    @Test
+    fun `should return next numbers in the same coroutine`() = runBlocking<Unit>(CounterContext()) {
+        assertEquals(0, coroutineContext[CounterContext]?.next())
+        assertEquals(1, coroutineContext[CounterContext]?.next())
+        assertEquals(2, coroutineContext[CounterContext]?.next())
+        assertEquals(3, coroutineContext[CounterContext]?.next())
+        assertEquals(4, coroutineContext[CounterContext]?.next())
+    }
+
+    @Test
+    fun `should have independent counter for each instance`() = runBlocking<Unit> {
+        launch(CounterContext()) {
+            assertEquals(0, coroutineContext[CounterContext]?.next())
+            assertEquals(1, coroutineContext[CounterContext]?.next())
+            assertEquals(2, coroutineContext[CounterContext]?.next())
+        }
+        launch(CounterContext()) {
+            assertEquals(0, coroutineContext[CounterContext]?.next())
+            assertEquals(1, coroutineContext[CounterContext]?.next())
+            assertEquals(2, coroutineContext[CounterContext]?.next())
+        }
+    }
+
+    @Test
+    fun `should propagate to the child`() = runBlocking<Unit>(CounterContext()) {
+        assertEquals(0, coroutineContext[CounterContext]?.next())
+        launch {
+            assertEquals(1, coroutineContext[CounterContext]?.next())
+            launch {
+                assertEquals(2, coroutineContext[CounterContext]?.next())
+            }
+            launch(CounterContext()) {
+                assertEquals(0, coroutineContext[CounterContext]?.next())
+                assertEquals(1, coroutineContext[CounterContext]?.next())
+                launch {
+                    assertEquals(2, coroutineContext[CounterContext]?.next())
+                }
+            }
+        }
+    }
 }
